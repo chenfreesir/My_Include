c47a4a016c77b5afd610ff770e97cbd229f3f6ee
chenfreesir <1261633857@qq.com>
2024-11-20 21:43:35 +0800

    [Title] First Update
    [desc_EN] Add power function calculate function
    [desc_CN] 增加幂函数计算函数

---
+++ README.md
@@ -0,0 +1,9 @@
+# My_Include
+
+* CHN<br>
+该项目用于保存各种的经典算法。<br>
+希望有朝一日能成为自己的Lib库文件。<br>
+
+* ENG<br>
+This project is used to preserve various classic algorithms.<br>
+Hope one day it can become myself's lib.
---
+++ math/Mymath.h
@@ -0,0 +1,9 @@
+/*---------------------------------------------*
+*--- Function: myPow                        ---*
+*--- Des: Calculate power function          ---*
+*--- Input                                  ---*
+*------ num: base number                 ------*
+*------ n: power                         ------*
+*--- Output: Calculation result             ---*
+*---------------------------------------------*/
+double myPow(double num, int n);
